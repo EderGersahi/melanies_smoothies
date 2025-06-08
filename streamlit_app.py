933701f8024499504e892957dfba3dfbcd0f8800
EderGersahi <103218544+EderGersahi@users.noreply.github.com>
2025-06-08 12:25:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 
+import requests
 from snowflake.snowpark.functions import col, when_matched
 
 cnx=st.connection("snowflake")
@@ -49,7 +50,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
-
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt = """
@@ -67,8 +69,4 @@ if ingredients_list:
 
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
 
-sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
