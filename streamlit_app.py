494e117b194aa286aaefd5e56b6795fe2f261b7a
EderGersahi <103218544+EderGersahi@users.noreply.github.com>
2025-06-08 11:46:02 -0600

    Update streamlit_app.py
    connected changes needed bc of move form SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
+
+cnx=st.connection("snowflake")
+session=cnx.session(
+
 
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write("Orders that need to be filled.")
